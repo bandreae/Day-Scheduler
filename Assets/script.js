d9f85a5d0081d78afd882c81fa5b719200c70c6f
andrbj <brandonjandreae@gmail.com>
2021-01-22 22:21:47 -0600

    getting current time in js

--- Assets/script.js
+++ Assets/script.js
@@ -1,4 +1,8 @@
 alert("hello world");
 
-$(document).ready()
+$(document).ready(function () {
+    var currentTime = moment().format("dddd, MMMM Do, YYYY");
+     // Friday, January 22nd, 2021
+$('#date').append(now);
 
+});
